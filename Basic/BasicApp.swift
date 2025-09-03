3cce4886b3ebe374ad4351e7f867a896eab55ff4
Parrie Vonveron <vonveron.pa@gmail.com>
2025-09-03 12:16:52 -0300

    Initial Commit

---
+++ Basic/BasicApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicApp.swift
+//  Basic
+//
+//  Created by Parrié on 03-09-2025.
+//
+
+import SwiftUI
+
+@main
+struct BasicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
