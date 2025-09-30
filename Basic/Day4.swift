417df2eefbd7df2647c8494a8083de25b15d7e85
Parrie Vonveron <vonveron.pa@gmail.com>
2025-09-30 15:16:36 -0300

    Day4 Text / Roundedrectangle done

--- Basic/Day4.swift
+++ Basic/Day4.swift
@@ -10,23 +10,29 @@ import SwiftUI
 struct Day4: View {
         
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/).padding().background(
+        RoundedRectangle(cornerRadius: 100)
+            .fill(Color.green)).foregroundColor(.yellow)
+            .frame(width: 175, height: 200)
+            .shadow(radius: 5)
         
     }
+    
+//    struct RoundRec: View {
+//        var body: some View{
+//            
+//            RoundedRectangle(cornerRadius: 25)
+//                .fill(Color.green)
+//                .frame(width: 75, height: 75,)
+//                .shadow(radius: 4)
+//            
+//        }
+//        
+//    }
     
 }
 
-struct RoundRec: View {
-    var body: some View{
-        
-        RoundedRectangle(cornerRadius: 15)
-            .frame(width: 55, height: 55,)
-            .fill(Color.blue)
-            .shadow(radius: 4)
-        
-    }
-    
-}
+
 
 #Preview {
     Day4()
