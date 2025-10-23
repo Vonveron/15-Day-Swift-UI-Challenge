91599a6379862fb04f4dc685fdae96194572cc91
Parrie Vonveron <vonveron.pa@gmail.com>
2025-10-22 22:29:37 -0300

    Day 5 completed, Day 6 started

---
+++ Basic/Day6.swift
@@ -0,0 +1,18 @@
+//
+//  Day6.swift
+//  Basic
+//
+//  Created by Parrié on 22-10-2025.
+//
+
+import SwiftUI
+
+struct Day6: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Day6()
+}
