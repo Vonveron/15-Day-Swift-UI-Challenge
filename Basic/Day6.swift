00ee6ab4bcabcee319dca841500f484090abb7bb
Parrie Vonveron <vonveron.pa@gmail.com>
2025-10-29 12:58:32 -0300

    Day 6 added funcs, and cleared ID's

--- Basic/Day6.swift
+++ Basic/Day6.swift
@@ -7,24 +7,27 @@
 
 import SwiftUI
 
-struct Habit: Identifiable {
-    let id = UUID()
+// Added codable below to convert data to and from JSON
+
+struct Habit: Identifiable, Codable {
+    let id: UUID
     var title: String
     var isDone: Bool
     
 }
-    
 
 struct HabitListView: View {
+    @AppStorage("habitsData") private var habitsData: Data = Data()
+    @State private var habits: [Habit] = []
     
-    @State private var habits = [
-        Habit(title: "Code", isDone: false),
-        Habit(title: "Gym", isDone: false),
-        Habit(title: "Meditate", isDone: false),
-        Habit(title: "Cook", isDone: false),
-        Habit(title: "Read", isDone: false),
-        
-    ]
+//    @State private var habits = [
+//        Habit(id: UUID(), title: "Code", isDone: false),
+//        Habit(id: UUID(), title: "Gym", isDone: false),
+//        Habit(id: UUID(), title: "Meditate", isDone: false),
+//        Habit(id: UUID(), title: "Cook", isDone: false),
+//        Habit(id: UUID(), title: "Read", isDone: false),
+//        
+//    ]
     var body: some View {
         NavigationView {
             List($habits) {$habits in
@@ -33,11 +36,36 @@ struct HabitListView: View {
                         .font(.headline)
                         .foregroundColor(habits.isDone ? .yellow : .primary)
                     
+                }.onChange(of: habits.isDone) { _ in saveHabits()
                 }
             }
         }.navigationTitle("My Habits")
+            .onAppear(perform: loadHabits)
     }
 }
+
+private func saveHabits() {
+    do {
+        let encoded = try JSONEncoder().encode(habits)
+        habitsData = encoded
+    }
+    catch {
+        print("Failed to save habits: \(error)")
+    }
+  }
+
+private func loadHabits() {
+    guard !habitsData.isEmpty else {
+        
+        Habit(id: UUID(), title: "Code", isDone: false),
+        Habit(id: UUID(), title: "Gym", isDone: false),
+        Habit(id: UUID(), title: "Meditate", isDone: false),
+        Habit(id: UUID(), title: "Cook", isDone: false),
+        Habit(id: UUID(), title: "Read", isDone: false),
+        
+    }
+}
+
 
 
 #Preview {
