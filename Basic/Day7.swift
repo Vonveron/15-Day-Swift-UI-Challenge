cb4ec67375bf7f79e36c796661cce3158c9df463
Parrie Vonveron <vonveron.pa@gmail.com>
2025-11-05 22:43:11 -0300

    Day 7 started

---
+++ Basic/Day7.swift
@@ -0,0 +1,24 @@
+//
+//  Day7.swift
+//  Basic
+//
+//  Created by V̅onveron on 05-11-2025.
+//
+
+import SwiftUI
+
+//struct Day7: View {
+//}
+
+struct RandomMealGenerator: View {
+    var body: some View {
+        
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+    }
+    
+}
+
+#Preview {
+    RandomMealGenerator()
+}
