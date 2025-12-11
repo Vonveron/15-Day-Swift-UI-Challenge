698fd2fcfe63ec2a18f0abdee5fa239d000ce16e
Parrie Vonveron <vonveron.pa@gmail.com>
2025-12-10 23:19:01 -0300

    Initial view enhancements

--- Swift 15/InitialScreenView.swift
+++ Swift 15/InitialScreenView.swift
@@ -79,50 +79,47 @@ struct SecondPage: View {
 
     var body: some View {
         NavigationStack {                         // iOS 16+: replace with NavigationView if needed
-            ScrollView {
-                NavigationStack {
-                    Text("")
-                        .toolbar {
-                            ToolbarItem(placement: .navigationBarTrailing) {
-                                Menu {
-                                    NavigationLink(destination: Day3()) {
-                                        Text("ToDo List")
-                                            .font(.headline)
-                                            .foregroundColor(.red)
-                                    }.padding()
-                                    
-                                    NavigationLink(destination: Day4()) {
-                                        Text("Weather").bold()
-                                            .foregroundColor(.green)
-                                    }.padding()
-                                    
-                                    NavigationLink(destination: Day5()) {
-                                        Text("Timer").bold()
-                                            .foregroundColor(.blue)
-                                    }.padding()
-                                    
-                                    NavigationLink(destination: Day6()) {
-                                        Text("Check List").bold()
-                                            .foregroundColor(.green)
-                                    }.padding()
-                                    
-                                    NavigationLink(destination: Day7()) {
-                                        Text("Meal Prep").bold()
-                                            .foregroundColor(.green)
-                                    }.padding()
-                                    
-                                    NavigationLink(destination: Day8()) {
-                                        Text("Music UI").bold()
-                                            .foregroundColor(.green)
-                                    }.padding()
+            NavigationStack {
+                Menu() {
+                                NavigationLink(destination: Day3()) {
+                                    Text("ToDo List")
+                                        .font(.headline)
+                                        .foregroundColor(.red)
+                                }.padding()
+                                
+                                NavigationLink(destination: Day4()) {
+                                    Text("Weather").bold()
+                                        .foregroundColor(.green)
+                                }.padding()
+                                
+                                NavigationLink(destination: Day5()) {
+                                    Text("Timer").bold()
+                                        .foregroundColor(.blue)
+                                }.padding()
+                                
+                                NavigationLink(destination: Day6()) {
+                                    Text("Check List").bold()
+                                        .foregroundColor(.green)
+                                }.padding()
+                                
+                                NavigationLink(destination: Day7()) {
+                                    Text("Meal Prep").bold()
+                                        .foregroundColor(.green)
+                                }.padding()
+                                
+                                NavigationLink(destination: Day8()) {
+                                    Text("Music UI").bold()
+                                        .foregroundColor(.green)
+                                }.padding()
 //
-                                } label: {
-                                    Image(systemName: "sidebar.right")
-                                        .font(.title2)
-                                }
+                            } label: {
+                                Image(systemName: "sidebar.right")
+                                    .font(.title2)
                             }
-                        }
-                }
+                        }.frame(alignment: .trailing)
+            ScrollView {
+//                        }
+//                }
                 
                 VStack(alignment: .leading, spacing: 20) {
                     
