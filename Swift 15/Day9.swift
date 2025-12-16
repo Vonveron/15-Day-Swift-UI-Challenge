4b316aa11b7be93ab6949e65f84cffc6d67c3ebf
Parrie Vonveron <vonveron.pa@gmail.com>
2025-12-16 09:53:04 -0300

    Day 9 continued

--- Swift 15/Day9.swift
+++ Swift 15/Day9.swift
@@ -13,18 +13,36 @@ struct Note: Identifiable, Codable {
     var content: String
 }
 
+struct NoteDetailView: View {
+    @Binding var note: Note
+
+        var body: some View {
+            TextEditor(text: $note.content)
+                .padding()
+                .navigationTitle(note.title)
+        }
+    
+}
 
 struct Day9: View {
+    
+    @Binding var note: Note
     
     @State private var note1: [Note] = [Note(id: UUID(), title: "First Note Test", content: "World") ]
     
     var body: some View {
         NavigationStack {
-            List(note1)  { note1 in
-                Text(note1.title)
+            List($note1)  { note1 in
+                NavigationLink(note.title) {
+                    NoteDetailView(note: $note)
+                }
+                TextEditor(text: $note.content)
+                    .padding()
+                    .navigationTitle(note.title)
             }
         }
     }
+    
 }
 #Preview {
     Day9()
